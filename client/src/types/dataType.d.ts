eb9e1ed7937024270f47c4ee8b1f51b1f6d92ce1
teklu abayneh <tekluabayneh@gmail.com>
2025-10-24 23:29:47 +0300

    refactor: flat types folder

---
+++ client/src/types/dataType.d.ts
@@ -0,0 +1,6 @@
+export type accentColorsType = {
+  cyan: { primary: string; glow: string };
+  purple: { primary: string; glow: string };
+  magenta: { primary: string; glow: string };
+  green: { primary: string; glow: string };
+};
