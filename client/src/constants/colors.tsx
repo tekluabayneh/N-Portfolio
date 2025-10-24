39a5eb7a23f696703d787c412fe9df040170093a
teklu abayneh <tekluabayneh@gmail.com>
2025-10-24 23:29:29 +0300

    refactor: flat colors constants

--- client/src/constants/colors.tsx
+++ client/src/constants/colors.tsx
@@ -1,7 +1,8 @@
-export const accentColors = {
-    cyan: { primary: "#00ffff", glow: "rgba(0, 255, 255, 0.5)" },
-    purple: { primary: "#a855f7", glow: "rgba(168, 85, 247, 0.5)" },
-    magenta: { primary: "#ec4899", glow: "rgba(236, 72, 153, 0.5)" },
-    green: { primary: "#10b981", glow: "rgba(16, 185, 129, 0.5)" },
-};
+import type { accentColorsType } from "../types/dataType";
 
+export const accentColors: accentColorsType = {
+  cyan: { primary: "#00ffff", glow: "rgba(0, 255, 255, 0.5)" },
+  purple: { primary: "#a855f7", glow: "rgba(168, 85, 247, 0.5)" },
+  magenta: { primary: "#ec4899", glow: "rgba(236, 72, 153, 0.5)" },
+  green: { primary: "#10b981", glow: "rgba(16, 185, 129, 0.5)" },
+};
