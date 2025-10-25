5ec659ef1fee9633f0cce0c4305f1a0c2876879f
teklu abayneh <tekluabayneh@gmail.com>
2025-10-25 23:27:14 +0300

    refactor: update data type definitions

--- client/src/types/dataType.d.ts
+++ client/src/types/dataType.d.ts
@@ -4,3 +4,49 @@ export type accentColorsType = {
   magenta: { primary: string; glow: string };
   green: { primary: string; glow: string };
 };
+export type CommandPropesType = {
+  onCommand: (input: type) => void;
+  accent: accentColorsType[0];
+  fontSize: string;
+  output?: string;
+};
+export type OutPutPropesType = {
+  accent: accentColorsType[0];
+  fontSize: string;
+  output: {
+    type: string;
+  };
+};
+
+export type TerminalcontentPropesType = {
+  commands;
+  terminalRef;
+  isMaximized;
+  accent: accentColorsType[0];
+  settings: settingStateType;
+  handleCommand;
+  setIsMaximized;
+};
+
+export type SettingPropesType = {
+  show;
+  onClose;
+  settings: settingStateType;
+  onSettingsChange;
+  accent: accentColorsType[0];
+};
+
+export type CommandType = {
+  input: string;
+  output: {
+    type: string;
+  };
+  timestamp: Date;
+};
+
+type AccentColor = keyof typeof accentColors;
+export type settingStateType = {
+  accentColor: AccentColor;
+  fontSize: "small" | "medium" | "large";
+  transparency: number;
+};
