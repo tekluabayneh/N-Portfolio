8176d34dff3ce5bc1429fee753d821d80c63da77
teklu abayneh <tekluabayneh@gmail.com>
2025-10-26 23:24:26 +0300

    feat(backend): add app module files

---
+++ backend/app/app.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/portfolio/2/routes"
+)
+
+type App struct {
+	router http.Handler
+}
+
+func New() *App {
+
+	app := &App{
+		router: routes.LoadRouter(),
+	}
+
+	return app
+
+}
+
+func (app *App) Start() {
+  PORT := os.Getenv("PORT")
+
+
+  if PORT == ""{ 
+PORT = "9090"
+  }
+
+	server := &http.Server{ 
+     Addr: ":" + PORT,
+	 Handler: app.router,
+	}
+fmt.Printf("Server running at http://localhost:%s\n", PORT)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+
+}
