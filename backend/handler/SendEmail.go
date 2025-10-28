3ce532a54af71b53b5459c3b98d0ba1d0ceab37d
teklu abayneh <tekluabayneh@gmail.com>
2025-10-29 00:22:55 +0300

    Improve email template and sending logic in SendEmail.go

--- backend/handler/SendEmail.go
+++ backend/handler/SendEmail.go
@@ -3,29 +3,23 @@ package handler
 import (
 	"fmt"
 	"net/http"
-"os"
-	"gopkg.in/gomail.v2"
+
+	"github.com/portfolio/2/service"
 )
 
 type SendEmailType struct{ 
 
 }
+type messageType struct{ 
+SenderName, SenderEmail, Message string
+}
 
 func (h *SendEmailType) SendEmail(w http.ResponseWriter, r *http.Request)  {
-	m := gomail.NewMessage()
-	m.SetHeader("From", "tekluabayneh@gmail.com")
-	m.SetHeader("To", "tekluabaynehupwork@gmail.com")
-	m.SetAddressHeader("Cc", "dan@example.com", "Dan")
-	m.SetHeader("Subject", "Hello!")
-	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
-	m.Attach("/home/Alex/lolcat.jpg")
-	password := os.
-	d := gomail.NewDialer("smtp.gmail.com", 587, "your_email@gmail.com", )
-
-	
-	// Send the email to Bob, Cora and Dan.
-	if err := d.DialAndSend(m); err != nil {
-		panic(err)
+	data := &messageType{
+		SenderName: "one",
+		SenderEmail: "tekluabayneh@gmail.com",
+		Message: "man i guess this work is getting better and better we don't give up right",
 	}
+   service.SendEmail(data.SenderName, data.SenderEmail, data.Message)
 	fmt.Println("test work")	
 }
