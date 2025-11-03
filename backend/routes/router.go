181c682021d19c63bf1f2be99d638395ff1ad414
Teklu <tekluabayneh@gmail.com>
2025-11-03 21:16:21 +0400

    Update router with new SendEmail endpoint

--- backend/routes/router.go
+++ backend/routes/router.go
@@ -9,10 +9,9 @@ import (
 	"github.com/portfolio/2/handler"
 )
 
+func LoadRouter() *chi.Mux {
 
-func LoadRouter() *chi.Mux{ 
-
-router := chi.NewRouter() 
+	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"*"},
@@ -25,22 +24,19 @@ router := chi.NewRouter()
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message":"opps! it's a live"}`))
+		w.Write([]byte(`{"message":"it's a live"}`))
 
 	})
-router.Route("/", func(r chi.Router){
- SendEmailRouter(r)
-})
+	router.Route("/", func(r chi.Router) {
+		SendEmailRouter(r)
+	})
 
-return router
+	return router
 }
 
+func SendEmailRouter(router chi.Router) {
+	SendEmailHandler := &handler.SendEmailType{}
 
-func SendEmailRouter(router chi.Router){ 
-	SendEmailHandler := &handler.SendEmailType{ 
-		
-	}
-	
-	router.Get("/SendEmail", SendEmailHandler.SendEmail)
-	 
+	router.Post("/SendEmail", SendEmailHandler.SendEmail)
+
 }
