ce2f78a9ff8be98ea4caaabb1a510f7ba67a8987
Teklu <tekluabayneh@gmail.com>
2025-11-03 21:16:22 +0400

    Wrap App with Toaster component for toast notifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Portfolio from "./pages/home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <Portfolio />
+<Toaster position="top-right" />
     </>
   );
 }
