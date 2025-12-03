e389a166f59bc46b1382423a2dc490c23b23cc13
Teklu <tekluabayneh@gmail.com>
2025-12-03 14:25:16 +0900

    fix resume path

--- client/src/components/terminal/ResumeDownloader.tsx
+++ client/src/components/terminal/ResumeDownloader.tsx
@@ -1,22 +1,33 @@
-function ResumeButton({accent}:{accent:{ 
-    primary:string,
-    glow:string
-}}) {
-  const handleDownload = () => {
-    const link = document.createElement("a");
-    link.href = "../../public/resume.pdf";
-    link.download = "Teklu_Resume.pdf"; 
-    link.click();
+const ResumeButton = ({ accent }: { accent: { primary: string; glow: string } }) => {
+  const downloadFiles = () => {
+    const files = [
+      { path: "/resume.pdf", name: "Teklu_Resume.pdf" },
+      { path: "/resume_alt.pdf", name: "Teklu_Resume_Alt.pdf" },
+    ];
+
+    files.forEach((file) => {
+      const link = document.createElement("a");
+      link.href = file.path;
+      link.download = file.name;
+      document.body.appendChild(link); 
+      link.click();
+      document.body.removeChild(link);
+    });
   };
 
-
-  return <button
-        className="px-6  cursor-pointer py-3 rounded-lg font-bold transition-all duration-300 hover:scale-105"
-        style={{
-          backgroundColor: accent.primary,
-          color: "#000",
-          boxShadow: `0 0 20px ${accent.glow}`,
-        }} onClick={handleDownload}>Download Resume</button>;
+  return (
+    <button
+      className="px-6 cursor-pointer py-3 rounded-lg font-bold transition-all duration-300 hover:scale-105"
+      style={{
+        backgroundColor: accent.primary,
+        color: "#000",
+        boxShadow: `0 0 20px ${accent.glow}`,
+      }}
+      onClick={downloadFiles} 
+    >
+      Download Resume
+    </button>
+  );
 }
 
-export default ResumeButton;
+export default ResumeButton
